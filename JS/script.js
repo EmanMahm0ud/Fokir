7943699148c0edb68f87207bde1634fb8d1ebb65
EmanMahm0ud <eman.talha2000@gmail.com>
2022-04-27 01:19:18 +0200

    Edit navbar & scrolling

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,20 @@
-const navbar = document.querySelector(".dash-container");
+const navbarDashes = document.querySelector(".dash-container");
 const navList = document.querySelector(".nav-menu ul");
+const homeSection = document.querySelector(".home")
+const arrowUp = document.querySelector(".arrow-up");
+const navbar = document.querySelector(".nav-bar");
 
+// open/close the navbar items when the dashed button is clicked
 function openMenu() {
-    console.log("nav")
     if (window.getComputedStyle(navList).display === "none") {
         navList.style.display = "block";
-        console.log("d-none");
     } else if (window.getComputedStyle(navList).display === "block") {
         navList.style.display = "none";
-        console.log("d-esle ", navList.style.display);
     }
 }
 
+// display the navbar items/dashed button based on screen width
 function resetNavbar() {
-    console.log(window.innerWidth);
     if (window.innerWidth > 1000) {
         navList.style.display = "flex";
     } else {
@@ -21,6 +22,31 @@ function resetNavbar() {
     }
 }
 
-navbar.addEventListener("click", openMenu);
+// display the arrow-up when the screen is far from the home section
+function showArrowUp() {
+    if (window.scrollY > homeSection.offsetHeight / 2) {
+        arrowUp.style.visibility = "visible";
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+        arrowUp.style.visibility = "hidden";
+    }
+}
+
+// change the navbar color based on the screen
+function editNavbarColor() {
+    if (window.scrollY == homeSection.offsetTop) {
+        navbar.style.backgroundColor = "transparent";
+    } else {
+        navbar.style.backgroundColor = "rgba(0, 0, 0, 0.788)";
+    }
+}
+
+navbarDashes.addEventListener("click", openMenu);
 
 window.addEventListener("resize", resetNavbar);
+
+window.addEventListener("scroll", () =>  {
+    showArrowUp();
+    editNavbarColor();
+});
